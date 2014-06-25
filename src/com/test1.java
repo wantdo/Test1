8c699d0b408032a7d89c716e19982e235d8de3a5
Administrator <Administrator@PC-20140528TYVZ>
2014-06-25 17:57:35 +0800

    zqh update 666666666

--- src/com/test1.java
+++ src/com/test1.java
@@ -8,7 +8,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1111111111111");
-		System.out.println("2222222444444");
+		System.out.println("6666666666666");
 
 	}
 
