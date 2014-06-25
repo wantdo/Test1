17df31e1a082ef55e9b3773df3d447390c04f1e3
wantdo <zheng_user@163.com>
2014-06-25 14:53:33 +0800

    Update test1.java

--- src/com/test1.java
+++ src/com/test1.java
@@ -8,6 +8,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1111111111111");
+		System.out.println("2222222222222");
 
 	}
 
